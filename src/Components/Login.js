0aae6c2cdaca25a6d83fef57b30d6042df300fee
Supanee Patsa <156906258+Supanee-patsa@users.noreply.github.com>
2024-05-01 11:29:46 +0200

    add form

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,41 +1,4 @@
 import "../index.css";
-import { useNavigate } from "react-router-dom";
-
-const Login = () => {
-    const [name, setName] = useState("");
-    const [password, setPassword] = useState("");
-
-    const navigate = useNavigate();
-
-    const checkUser = (users) => {
-      const user = users.find(
-        (user) => user.Name === Name && user.password === password);
-        console.log(user)
-      if (user.name === name && user.password === password) return user;
-    };
-
-    const handleSubmit = async (e) => {
-        e.preventDefault ();
-
-        if (name ==="" || password === "") {
-            alert ("All fields are required!");
-        }
-
-        const user = await axios
-        .get("/users")
-        .then((res) => checkUser(res.data, name, password))
-        .catch ((error) => {
-            console.log(error)
-        })
-
-        if (user.name === name && user.password === password) {
-            navigate("/");
-
-            locslStorage.setItem("user", JSON.stringify(user.id));
-        }
-        setName("");
-        setPassword("");
-    }
 
     return (
         <div className="container">
