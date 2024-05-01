cbf01a99bf3ece3c4e786746a1ef589b0f4723a7
Supanee Patsa <156906258+Supanee-patsa@users.noreply.github.com>
2024-05-01 11:17:26 +0200

    change

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" lement={<Login />} />
       </Routes>
