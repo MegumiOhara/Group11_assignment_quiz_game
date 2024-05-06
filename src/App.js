4a02c28a235b7b9dd3c173b29605fe88527f85a4
Supanee Patsa <156906258+Supanee-patsa@users.noreply.github.com>
2024-05-07 00:22:02 +0700

    add app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import Login from './Components/Login';
-import Register from './Components/Register';
+import "./App.css";
+import LoginButton from "./components/LoginButton";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./pages/Home";
+import { useAuth0 } from "@auth0/auth0-react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
-function App() {
+const App = () => {
+  const { isAuthenticated } = useAuth0();
+
   return (
-    <div>
-      <Login />
-      <Register />
-    </div>
-
+    <Router>
+      <div>
+        <Navbar />
+        {isAuthenticated ? (
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        ) : (
+          <LoginButton />
+        )}
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
