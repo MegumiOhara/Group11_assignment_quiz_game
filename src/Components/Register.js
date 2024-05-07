71cf77980082f312a10de33398ca4782b0c421f0
Supanee Patsa <156906258+Supanee-patsa@users.noreply.github.com>
2024-05-08 01:03:59 +0700

    add form

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,33 +1,58 @@
-import "../index.css";
-const Register = () => {
-    return (
-        <div className="container">
-             <form>
-               <h1>Register</h1>
-               <label>
-                   <input type="text"
-                     placeholder="Username"  />   
-                     
-              </label>
-                <label>
-                    <input type="email"
-                         placeholder="Email"  />
-                         
-                </label>
-                <label>
-                    <input type="password"
-                         placeholder="Password"   />   
-                             
-                </label>
-                <button className="btn" type="submit"
-                 >
-                
-                    <p>Submit</p>     
-                </button>
-              </form>
-    </div>  
+import axios from "axios";
+import { useState } from "react";
 
+
+ const Register = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const checkEmail = (users) => {
+    const user = users.find((user) => user.email === email);
+    if (user) {
+      console.log("Email already exists");
+      return user;
+    }
+  };
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const users = await axios.get("http://localhost:6001/users").then((res) => checkEmail(res.data, email ));
+    
+    if (users) {
+      alert("Email already exists!");
+    } else {
+      const user = { username, email, password };
+      await axios.post("http://localhost:6001/users", user).then(alert("User registered"));
+    }  
+    setUsername("");
+    setEmail("");
+    setPassword("");  
+  };
+
+  
+
+  return (
+      <div >
+        <form>
+          <h1>Register</h1>
+              <input type="text"
+                  placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)}  />   
+                      
+              <input type="email"
+                  placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}  />
+                          
+              <input type="password"
+                  placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}  />   
+                              
+                  
+              <button className="btn" type="submit" onClick={handleSubmit}>
+                Submit   
+              </button>
+          </form>
+        </div>
 );
-}
-export default Register;
+};
 
+export default Register;
