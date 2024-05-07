c03ce6d3a3f5ab11d4c2202098c68bd5078f82f5
Supanee Patsa <156906258+Supanee-patsa@users.noreply.github.com>
2024-05-08 01:04:36 +0700

    add form

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-import "./App.css";
-import LoginButton from "./components/LoginButton";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Home from "./pages/Home";
-import { useAuth0 } from "@auth0/auth0-react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import React from 'react';
+import Home from './components/Home';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Register from '/components/Register';
+import Login from '/components/Login';
+import Navbar from '/components/Navbar';
+import PrivateRoute from '/components/PrivateRoute';
+import Welcome from '/components/Welcome';
 
-const App = () => {
-  const { isAuthenticated } = useAuth0();
-
+function App() {
   return (
-    <Router>
-      <div>
-        <Navbar />
-        {isAuthenticated ? (
-          <Routes>
-            <Route path="/" element={<Home />} />
-          </Routes>
-        ) : (
-          <LoginButton />
-        )}
-      </div>
-    </Router>
+
+    <div >
+      <BrowserRouter>
+      <Navbar />
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/welcome" element={<Welcome />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="user" element={<PrivateRoute>
+            <Route path="welcome" element={<Welcome />} />
+          </PrivateRoute>} />
+      </Routes> 
+      
+    </BrowserRouter>   
+    </div>
   );
-};
+}
 
 export default App;
