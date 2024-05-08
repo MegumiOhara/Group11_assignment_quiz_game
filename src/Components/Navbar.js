66d64d7f19473d27c4e0d12e7d2d9794f994bb8f
Supanee Patsa <156906258+Supanee-patsa@users.noreply.github.com>
2024-05-08 19:35:40 +0700

    modified

---
+++ src/Components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Navbar() {
+
+  return (
+    
+  <nav className="nav">
+      <a href="/" >Quiz Game </a>
+     
+        <a href="/Register" >Register </a>
+
+        <a href="/Login" >Login </a>
+         
+    </nav>
+  ); 
+}
