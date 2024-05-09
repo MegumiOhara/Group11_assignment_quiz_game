efd9710416bfaeb0d3777543cb67935e975140b7
Supanee Patsa <156906258+Supanee-patsa@users.noreply.github.com>
2024-05-09 07:22:45 +0000

    Add AppTanja.js

---
+++ src/components/AppTanja.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import './indexTanja.css';
+import Result from './components/Result';
+import Game from './components/Level-2-page/Game';
+import { useState } from 'react'
+import {questions} from './components/Level-2-page/clothes-questions'
+
+function AppTanja() {
+  const [step, setStep] = useState(0);
+  const [correct, setCorrect] = useState(0);
+  const question = questions[step];
+
+ const onClickVariant = (index) => {
+    console.log(step, index);
+    setStep(step + 1);
+
+    if (index === question.correct) {
+      setCorrect(correct + 1);
+    }
+ }
+
+  return (
+    <div className="container">
+      <div className="App">
+        {
+          step !== questions.length ? (
+          <Game  question={question} onClickVariant={onClickVariant}/>
+          ) : (
+          <Result correct={correct}/>
+          )}
+      </div>
+    </div>
+    );
+}
+
+export default AppTanja;
