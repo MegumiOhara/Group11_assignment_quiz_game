59e72803dece35be3a3c9108a4db13b1f188f099
Tanja Kiseleva <lessy_92@mail.ru>
2024-05-13 09:00:22 +0200

    change "a href" to <Link to=""> at Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
 
   return (
     
   <nav className="nav">
-      <a href="/" >Quiz Game </a>
+    <ul>
+      <li><Link to="/">Quiz Game</Link></li>
+    </ul>
      
         <a href="/Register" >Register </a>
 
